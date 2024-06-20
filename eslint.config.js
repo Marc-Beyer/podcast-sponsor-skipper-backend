ede883e6a9d8788b6de2bc8f76e322a48545489b
Marc Beyer <marc.beyer1@gmx.de>
2024-06-20 15:49:01 +0200

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: {
+                ...globals.node,
+                myCustomGlobal: "readonly",
+            },
+        },
+    }
+);
